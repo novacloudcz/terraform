a02d1fd81ec042f9450e8d6390313e8817164be2
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2020-11-18 16:32:57 +0100

    Add graphql-files-s3

---
+++ graphql-files-s3/lambda.tf
@@ -0,0 +1,23 @@
+resource "aws_lambda_function" "lambda" {
+  s3_bucket     = "novacloud-lambda-functions"
+  s3_key        = "github.com/graphql-services/graphql-files-s3/${var.lambda_version}/lambda.zip"
+  function_name = "${var.name}"
+
+  role        = "${var.role}"
+  handler     = "main"
+  runtime     = "go1.x"
+  publish     = true
+  timeout     = 30
+  memory_size = 1024
+
+  environment {
+    variables = "${var.environment_variables}"
+  }
+
+  vpc_config {
+    subnet_ids         = ["${var.vpc_subnet_ids}"]
+    security_group_ids = ["${var.vpc_security_group_ids}"]
+  }
+
+  tags = "${var.tags}"
+}
