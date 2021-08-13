5fbef9da017c926397390051f0a36c2ea94c1be1
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2021-08-13 13:29:31 +0200

    Update graphql-gateway to nodejs12.x

--- graphql-gateway/lambda.tf
+++ graphql-gateway/lambda.tf
@@ -1,22 +1,22 @@
 resource "aws_lambda_function" "lambda" {
   s3_bucket     = "novacloud-lambda-functions"
   s3_key        = "github.com/graphql-services/graphql-gateway/${var.lambda_version}/lambda.zip"
-  function_name = "${var.name}"
+  function_name = var.name
 
-  role        = "${aws_iam_role.lambda_exec.arn}"
+  role        = aws_iam_role.lambda_exec.arn
   handler     = "lambda.handler"
-  runtime     = "nodejs10.x"
+  runtime     = "nodejs12.x"
   publish     = true
   timeout     = 30
   memory_size = 1024
 
   environment {
-    variables = "${var.environment_variables}"
+    variables = var.environment_variables
   }
 
-  tags = "${var.tags}"
+  tags = var.tags
 
   tracing_config {
-    mode = "${var.tracing_config}"
+    mode = var.tracing_config
   }
 }
